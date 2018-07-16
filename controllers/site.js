4aff3883e3813430ec6e74f3d180699edc50baa0
xbc <you@example.com>
2018-07-16 17:46:18 +0800

    update

--- controllers/site.js
+++ controllers/site.js
@@ -30,7 +30,7 @@ exports.index = function (req, res, next) {
   // 取主题
   var query = {};
   if (!tab || tab === 'all') {
-    query.tab = {$nin: ['job', 'dev']}
+    //query.tab = {$nin: ['job', 'dev']}
   } else {
     if (tab === 'good') {
       query.good = true;
